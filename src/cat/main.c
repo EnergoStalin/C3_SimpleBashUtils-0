d3783c851cad6dd49d203ff024f4b1ef69837ac1
EnergoStalin <alex.vipers@mail.ru>
2023-03-03 21:51:12 +0300

    fix style & return code cat

--- src/cat/main.c
+++ src/cat/main.c
@@ -8,7 +8,7 @@ int main(int argc, char *const *argv) {
   ReturnCode return_code = OK;
   CatConfig config = {0};
 
-  ProcessArgs(argc, argv, &config);
+  return_code = ProcessArgs(argc, argv, &config);
 
   if (return_code != OK && return_code != INVALID_ARGUMENTS) {
     printf("ERROR: %s\n", GetCodeName(return_code));
--- src/grep/process_args.c
+++ src/grep/process_args.c
@@ -70,10 +70,10 @@ ReturnCode ProcessArgs(int argc, char *const *argv, GrepConfig *config) {
     if (!config->regexp && !config->regexp_file) {
       config->pattern = vect_at_char_ptr(files, 0);
       i = 1;
-      if(files->size == 2) {
+      if (files->size == 2) {
         config->no_filename = 1;
       }
-    } else if(files->size == 1) {
+    } else if (files->size == 1) {
       config->no_filename = 1;
     }
     for (; i < files->size && return_code == OK; i++) {
